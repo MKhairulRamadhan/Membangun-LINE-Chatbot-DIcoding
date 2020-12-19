19a6f8b046f5a11fec8874af7103b0ce02d40652
M Khairul Ramadhan <khairulr59@gmail.com>
2020-12-19 22:20:05 +0700

    Add string framework

---
+++ implementasiFIturJava/implementasiFIturJava/src/main/java/com/khairul/implementasiFIturJava/ImplementasiFIturJavaApplication.java
@@ -0,0 +1,13 @@
+package com.khairul.implementasiFIturJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementasiFIturJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementasiFIturJavaApplication.class, args);
+	}
+
+}
---
+++ implementasiFIturJava/implementasiFIturJava/src/test/java/com/khairul/implementasiFIturJava/ImplementasiFIturJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khairul.implementasiFIturJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementasiFIturJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
