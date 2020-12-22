dac80ccde0c1bd711f2c9c700dded21f6122c5a0
M Khairul Ramadhan <khairulr59@gmail.com>
2020-12-23 06:51:50 +0700

    Add spring boot template, TemanNgoding

---
+++ Teman/Teman/src/main/java/com/khairul/Teman/TemanApplication.java
@@ -0,0 +1,13 @@
+package com.khairul.Teman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemanApplication.class, args);
+	}
+
+}
---
+++ Teman/Teman/src/test/java/com/khairul/Teman/TemanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khairul.Teman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
